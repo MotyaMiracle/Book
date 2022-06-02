007ab307e04b6512637f7e82d2b1aeef2dc15034
Matvey Miracle <steammmm.volvo@mail.ru>
2022-06-02 21:56:41 +0300

    Succeed

--- ExampleInBook/Program.cs
+++ ExampleInBook/Program.cs
@@ -9,9 +9,28 @@ namespace ExampleInBook
 {
     class Program
     {
+        static string binary_search(int[] list, int item)
+        {
+            int low = 0;
+            int high = list.Length - 1;
+
+            while (low <= high)
+            {
+                int mid = (low + high) / 2;
+                int guess = list[mid];
+                if (guess == item) return Convert.ToString(mid);
+                if (guess > item) high = mid - 1;
+                else low = mid + 1;
+            }
+            return "None";
+
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] my_list = new int[] { 1, 3, 5, 7, 9 };
+
+            Console.WriteLine(binary_search(my_list, 3));
+            Console.WriteLine(binary_search(my_list, -1));
         }
     }
 }
