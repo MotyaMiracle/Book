e62441242764b364aa64c54187fde0c1c42a3709
Matvey Miracle <steammmm.volvo@mail.ru>
2022-06-05 13:27:27 +0300

    Succeed

--- ExampleInBook/Program.cs
+++ ExampleInBook/Program.cs
@@ -9,9 +9,37 @@ namespace ExampleInBook
 {
     class Program
     {
+        static int[] SelectionSort(int[] array)
+        {
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                //поиск минимального числа
+                int min = i;
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j] < array[min])
+                    {
+                        min = j;
+                    }
+                }
+                //обмен элементов
+                int temp = array[min];
+                array[min] = array[i];
+                array[i] = temp;
+            }
+            return array;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] my_array = new int[] { 5, 3, 6, 2, 10 };
+            Console.WriteLine("Отсортированный массив:");
+            SelectionSort(my_array);
+            for (int i = 0; i < my_array.Length; i++)
+            {
+                Console.WriteLine(my_array[i]);
+            }
+            Console.ReadLine();
         }
     }
 }
