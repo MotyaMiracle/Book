3c3ecdcb8161b8208648520904477408bb4469ee
motyamiracle@gmail.com <motyamiracle@gmail.com>
2022-06-08 19:04:53 +0300

    Succeed

--- ExampleInBook/Program.cs
+++ ExampleInBook/Program.cs
@@ -9,9 +9,37 @@ namespace ExampleInBook
 {
     class Program
     {
+        static void Countdown(int i)
+        {
+            Console.WriteLine(i); 
+            if (i <= 1) return;     // Базовый случай
+            else Countdown(i - 1); // Рекурсивный случай
+        }
+        static void Greet(string name)
+        {
+            Console.WriteLine("Hello, " + name + "!");
+            Greet2(name);
+            Console.WriteLine("Getting ready to say bye...");
+            Bye();
+        }
+        static void Greet2(string name)
+        {
+            Console.WriteLine("How are you, " + name + "?");
+        }
+        static void Bye()
+        {
+            Console.WriteLine("Ok bye!");
+        }
+        static int Fact(int x)
+        {
+            if (x == 1) return 1;
+            else return x * Fact(x - 1);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Countdown(3);
+            Greet("Maggie");
+            Console.WriteLine(Fact(3));
         }
     }
 }
