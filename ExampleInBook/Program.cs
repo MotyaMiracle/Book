b3f15258fc16b90d1cd185a8e7031391dc566494
motyamiracle@gmail.com <motyamiracle@gmail.com>
2022-06-15 15:20:58 +0300

    Succeed

--- ExampleInBook/Program.cs
+++ ExampleInBook/Program.cs
@@ -9,9 +9,31 @@ namespace ExampleInBook
 {
     class Program
     {
+        static Dictionary<string, bool> voted = new Dictionary<string, bool>();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var book = new Dictionary<string, double>();
+            book.Add("apple", 0.67);
+            book.Add("milk", 1.49);
+            book.Add("avocado", 1.49);
+
+            foreach (var item in book)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value}");
+            }
+            CheckVoter("Tom");
+            CheckVoter("Mike");
+            CheckVoter("Mike");
         }
-    }
+        static void CheckVoter(string Name)
+        {
+            if (voted.ContainsKey(Name)) Console.WriteLine("Kick them out!");
+            else
+            {
+                voted.Add(Name, true);
+                Console.WriteLine("Let them vote!");
+            }
+
+        }
+    }    
 }
