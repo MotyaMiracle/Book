3b49d8f087c95130dd057f0ecfb9b149a1f1a65b
MotyaMiracle <MotyaMiracle@gmail.com>
2022-06-23 11:02:56 +0300

    Succeed

--- ExampleInBook/Program.cs
+++ ExampleInBook/Program.cs
@@ -9,9 +9,46 @@ namespace ExampleInBook
 {
     class Program
     {
+        static Dictionary<string, string[]> graph = new Dictionary<string, string[]>();
+        static bool Search(string name)
+        {
+            var searchQueue = new Queue<string>(graph[name]);
+            var searched = new List<string>();
+            while (searchQueue.Any())
+            {
+                var person = searchQueue.Dequeue();
+                if (!searched.Contains(person))
+                {
+                    if (PersonIsSeller(person))
+                    {
+                        Console.WriteLine(person + " is a mango seller!");
+                        return true;
+                    }
+                    else
+                    {
+                        searchQueue = new Queue<string>(searchQueue.Concat(graph[person]));
+                        searched.Add(person);
+                    }
+                }                
+            }
+            return false;
+        }
+        static bool PersonIsSeller(string name)
+        {
+            return name.EndsWith("m");
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            graph.Add("you", new[] {"alice", "bob", "claire"});
+            graph.Add("bob", new[] {"anuj", "peggy"});
+            graph.Add("alice", new[] {"peggy"});
+            graph.Add("claire", new[] {"thom", "jonny"});
+            graph.Add("anuj", Array.Empty<string>());
+            graph.Add("peggy", Array.Empty<string>());
+            graph.Add("thom", Array.Empty<string>());
+            graph.Add("jonny", Array.Empty<string>());
+            Search("you");
         }
     }
 }
