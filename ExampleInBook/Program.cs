26d866b6c91c38503fc66a0ed928e3529b55c31d
MotyaMiracle <MotyaMiracle@gmail.com>
2022-06-25 11:56:20 +0300

    Succeed

--- ExampleInBook/Program.cs
+++ ExampleInBook/Program.cs
@@ -11,7 +11,14 @@ namespace ExampleInBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (word_a[i] == word_b[j])
+            {
+                cell[i][j] == cell[i - 1][j - 1] + 1;
+            }
+            else
+            {
+                cell[i][j] = Math.Max(cell[i - 1][j], cell[i][j - 1]);
+            }
         }
     }
 }
